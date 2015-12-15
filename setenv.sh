2dcb815d01cca51d327c2712c2335f6b3fbdc102
Lorenzo Manganaro <manganaro.lorenzo@hotmail.it>
2015-12-15 12:05:09 +0100

    Added "setenv.sh" file

---
+++ setenv.sh
@@ -0,0 +1,9 @@
+#
+# Modify with your own path to this directory
+#
+
+install_folder=$HOME/path_to_this_directory/
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$install_folder/lib
+export DATA=$install_folder/data
+export PATH=$PATH:$install_folder
